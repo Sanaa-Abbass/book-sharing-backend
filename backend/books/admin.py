606e5c7a0a0be3df4b791ce63a32adccad733131
Sanaa-Abbas <ssmohamedahmedaabbas@gmail.com>
2026-06-30 14:55:30 +0400

    Update the books adminl

--- backend/books/admin.py
+++ backend/books/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "author",
+        "owner",
+        "available",
+        "condition"
+    )
+
+    search_fields = (
+        "title",
+        "author",
+        "owner__username"
+    )
+
+    list_filter = (
+        "available",
+        "condition",
+        "language",
+        "category"
+    )
